b97b28873b19329908b25776b7d3de73204b1ff3
woroniecki <dwrona94@gmail.com>
2025-06-10 17:47:46 +0200

    Added purchase properties

--- FlightSalesSystem/FlightSalesSystem.Domain/Purchases/Purchase.cs
+++ FlightSalesSystem/FlightSalesSystem.Domain/Purchases/Purchase.cs
@@ -1,4 +1,6 @@
 ﻿using FlightSalesSystem.Domain.Abstractions;
+using FlightSalesSystem.Domain.Common;
+using FlightSalesSystem.Domain.Purchases.ValueObjects;
 
 namespace FlightSalesSystem.Domain.Purchases;
 public class Purchase : AggregateRoot
@@ -6,4 +8,10 @@ public class Purchase : AggregateRoot
     public Purchase(Guid id) : base(id)
     {
     }
+
+    public Guid FlightId { get; private set; }
+    public Guid TenantId { get; private set; }
+    public Money AbsolutePrice { get; private set; }
+    public Money FinalPrice { get; private set; }
+    public CustomerData CustomerData { get; private set; }
 }
---
+++ FlightSalesSystem/FlightSalesSystem.Domain/Purchases/ValueObjects/CustomerData.cs
@@ -0,0 +1,10 @@
+﻿using FlightSalesSystem.Domain.Abstractions;
+
+namespace FlightSalesSystem.Domain.Purchases.ValueObjects;
+public class CustomerData : ValueObject
+{
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        throw new NotImplementedException();
+    }
+}
